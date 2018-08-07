e4b713a9ba7991083e04a48c738da91398158ac7
Parker Siroishka <parkersir10@hotmail.com>
2018-08-06 21:36:35 -0600

    Firebase commit

--- src/app/profile/profile.ts
+++ src/app/profile/profile.ts
@@ -1,8 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { AngularFireDatabase } from 'angularfire2/database';
 
 @Component({
   selector: 'app-profile',
   templateUrl: 'profile.page.html',
   styleUrls: ['profile.page.scss']
 })
-export class ProfilePage {}
+export class ProfilePage implements OnInit {
+
+  userName$: Observable<any>
+
+  constructor(public db: AngularFireDatabase) {} 
+
+  getCurrentUser(): Observable<any>{
+      return this.db.object('user/Jason/name')
+        .valueChanges()
+  }
+
+  ngOnInit(): void {
+    this.userName$ = this.getCurrentUser();
+  }
+
+}
+
+
+
